1cb44187dba83f1371d75c0922fa81b1d4ba7d0b
Miłosz Marchewka <milosz77665@gmail.com>
2026-02-10 20:41:50 +0100

    fix vscode keyring issue

--- system/secrets.nix
+++ system/secrets.nix
@@ -1,5 +1,10 @@
+{ pkgs, ... }:
 {
   services.gnome.gnome-keyring.enable = true;
 
   security.polkit.enable = true;
+
+  security.pam.services.login.enableGnomeKeyring = true;
+
+  environment.systemPackages = [ pkgs.libsecret ];
 }
