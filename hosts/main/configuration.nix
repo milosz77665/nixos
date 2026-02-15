f5507bcb60544cb9f9830da2974b78a724429dd9
Miłosz Marchewka <milosz77665@gmail.com>
2026-02-15 13:15:54 +0100

    add garbage collection config to main host

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -3,6 +3,12 @@
   imports = [
     ./hardware-configuration.nix
   ];
+  
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.11";
