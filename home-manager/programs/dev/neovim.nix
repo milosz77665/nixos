07404d8126df41862ce9aff63beaa4fcb8a0e76c
Miłosz Marchewka <milosz77665@gmail.com>
2026-05-24 18:25:07 +0000

    remove gcc from neovim.nix

--- home-manager/programs/dev/neovim.nix
+++ home-manager/programs/dev/neovim.nix
@@ -1,12 +1,12 @@
 { pkgs, pkgsUnstable, ... }:
 {
-  home.packages = with pkgsUnstable; [
+  home.packages = with pkgs; [
     neovim 
     tree-sitter
-    gcc
     # lang servers
     gopls
     typescript-language-server 
     vue-language-server   
+    lua-language-server
   ];
 }
