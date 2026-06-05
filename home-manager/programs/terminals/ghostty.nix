f99fab972b71cc84e38affedca1fc3d84e8c4778
Miłosz Marchewka <milosz77665@gmail.com>
2026-06-05 12:05:29 +0200

    add ghostty settings

--- home-manager/programs/terminals/ghostty.nix
+++ home-manager/programs/terminals/ghostty.nix
@@ -1,3 +1,13 @@
 {
-  programs.ghostty.enable = true;
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "Banana Blueberry";
+
+      window-inherit-working-directory = true;
+      window-decoration = false;
+      window-padding-x = 4;
+      window-padding-y = 4;
+    };
+  };
 }
