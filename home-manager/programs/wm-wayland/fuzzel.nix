0d3a7e96799c98cc75716261101de36179e7e5d2
Miłosz Marchewka <milosz77665@gmail.com>
2026-06-06 16:28:20 +0200

    add fuzzel settings

--- home-manager/programs/wm-wayland/fuzzel.nix
+++ home-manager/programs/wm-wayland/fuzzel.nix
@@ -1,3 +1,33 @@
+{ pkgs, ... }:
 {
-  programs.fuzzel.enable = true;
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        font = "JetBrainsMono Nerd Font:size=13";
+        terminal = "${pkgs.ghostty}/bin/ghostty -e";
+        prompt = "❯  ";
+        icon-theme = "Papirus-Dark";
+        layer = "overlay";
+        lines = 10;
+        width = 40;
+        horizontal-pad = 20;
+        vertical-pad = 20;
+        inner-pad = 10;
+      };
+      colors = {
+        background = "191328ff";
+        text = "ccccccff";
+        match = "f6eb61ff";
+        selection = "392a48ff";
+        selection-text = "ffffffff";
+        selection-match = "f6eb61ff";
+        border = "22a2c9ff";
+      };
+      border = {
+        width = 2;
+        radius = 12;
+      };
+    };
+  };
 }
