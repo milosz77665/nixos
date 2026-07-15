e928d31dc2fea65eadba45f83436fa3d7ee62039
Miłosz Marchewka <milosz77665@gmail.com>
2026-07-15 14:09:09 +0000

    migrate pavucontrol to use mkEnableOption

--- home-manager/programs/audio/pavucontrol.nix
+++ home-manager/programs/audio/pavucontrol.nix
@@ -1,6 +1,21 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    pavucontrol
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.usr.audio.pavucontrol;
+in
+{
+  options.usr.audio.pavucontrol = {
+    enable = lib.mkEnableOption "PulseAudio Volume Control (pavucontrol)";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      pavucontrol
+    ];
+  };
 }
