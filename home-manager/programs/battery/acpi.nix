f6dc3f9bd644ddab35ceebf74f37a43bdd0c7efa
Miłosz Marchewka <milosz77665@gmail.com>
2026-07-15 14:09:29 +0000

    migrate acpi to use mkEnableOption

--- home-manager/programs/battery/acpi.nix
+++ home-manager/programs/battery/acpi.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    acpi
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.battery.acpi;
+in
+{
+  options.usr.battery.acpi = {
+    enable = lib.mkEnableOption "Advanced Configuration and Power Interface (ACPI)";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      acpi
+    ];
+  };
 }
