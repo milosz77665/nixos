12167ea938d2257266382a184bf15b88c83b2bbf
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:26:27 +0000

    migrate printing to use mkEnableOption

--- system/printing.nix
+++ system/printing.nix
@@ -1,12 +1,27 @@
-{ pkgs, ... }:
 {
-environment.systemPackages = with pkgs; [
-    system-config-printer
-    hplipWithPlugin 
-  ];
-  
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.hplipWithPlugin ]; 
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.sys.printing.hp;
+in
+{
+  options.sys.printing.hp = {
+    enable = lib.mkEnableOption "HP printing";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      system-config-printer
+      hplipWithPlugin
+    ];
+
+    services.printing = {
+      enable = true;
+      drivers = [ pkgs.hplipWithPlugin ];
+    };
   };
 }
+
