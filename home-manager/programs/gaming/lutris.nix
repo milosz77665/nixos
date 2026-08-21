60cdac316507357a738950dd590020de10b818fb
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:57:25 +0000

    migrate lutris to use mkEnableOption

--- home-manager/programs/gaming/lutris.nix
+++ home-manager/programs/gaming/lutris.nix
@@ -1,3 +1,17 @@
 {
-  programs.lutris.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.gaming.lutris;
+in
+{
+  options.usr.gaming.lutris = {
+    enable = lib.mkEnableOption "Lutris";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.lutris.enable = true;
+  };
 }
