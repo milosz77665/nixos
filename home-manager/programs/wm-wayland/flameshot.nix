88aca942094a2f5cac98e8e78afb2230c0d2e33b
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:07:12 +0000

    migrate flameshot to use mkEnableOption

--- home-manager/programs/wm-wayland/flameshot.nix
+++ home-manager/programs/wm-wayland/flameshot.nix
@@ -1,21 +1,36 @@
-{ pkgs, userConfig, ... }:
 {
-  services.flameshot = {
-    enable = true;
-    package = pkgs.flameshot;
-
-    settings = {
-      General = {
-        disabledTrayIcon = true;
-        showStartupLaunchMessage = false;
-        savePath = "${userConfig.homeDirectory}/Pictures/Screenshots";
-        savePathFixed = true;
-        useGrimAdapter = true;
-      };
-    };
+  config,
+  lib,
+  pkgs,
+  userConfig,
+  ...
+}:
+let
+  cfg = config.usr.wm-wayland.flameshot;
+in
+{
+  options.usr.wm-wayland.flameshot = {
+    enable = lib.mkEnableOption "Flameshot";
   };
 
-  home.packages = with pkgs; [
-    grim
-  ];
+  config = lib.mkIf cfg.enable {
+    services.flameshot = {
+      enable = true;
+      package = pkgs.flameshot;
+
+      settings = {
+        General = {
+          disabledTrayIcon = true;
+          showStartupLaunchMessage = false;
+          savePath = "${userConfig.homeDirectory}/Pictures/Screenshots";
+          savePathFixed = true;
+          useGrimAdapter = true;
+        };
+      };
+    };
+
+    home.packages = with pkgs; [
+      grim
+    ];
+  };
 }
