3a29be9b369addf3c8e7a345c285b68b3d0b1c84
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:25:05 +0000

    migrate battery to use mkEnableOption

--- system/battery.nix
+++ system/battery.nix
@@ -1,11 +1,24 @@
-{ pkgs, ... }:
 {
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-      CPU_ENERGY_PERF_POLICY_ON_SAV = "power";
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.sys.battery;
+in
+{
+  options.sys.battery = {
+    enable = lib.mkEnableOption "Battery";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_SAV = "power";
+      };
     };
   };
 }
