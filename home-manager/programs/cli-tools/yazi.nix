f0d2fe9ec14287960af7063206cf1be5145ed13f
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:49:24 +0000

    migrate yazi to use mkEnableOption

--- home-manager/programs/cli-tools/yazi.nix
+++ home-manager/programs/cli-tools/yazi.nix
@@ -1,7 +1,21 @@
 {
-  programs.yazi = {
-    enable = true;
-    enableBashIntegration = true;
-    shellWrapperName = "y";
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.cli-tools.yazi;
+in
+{
+  options.usr.cli-tools.yazi = {
+    enable = lib.mkEnableOption "Yazi";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.yazi = {
+      enable = true;
+      enableBashIntegration = true;
+      shellWrapperName = "y";
+    };
   };
 }
