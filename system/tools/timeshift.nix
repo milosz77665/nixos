007e167d4b94bc3317b4dee08b890eb674bdea1f
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:22:30 +0000

    migrate timeshift to use mkEnableOption

--- system/tools/timeshift.nix
+++ system/tools/timeshift.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    timeshift
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.sys.tools.timeshift;
+in
+{
+  options.sys.tools.timeshift = {
+    enable = lib.mkEnableOption "Timeshift";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      timeshift
+    ];
+  };
 }
