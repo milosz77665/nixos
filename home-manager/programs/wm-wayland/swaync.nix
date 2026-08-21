1d8a65d809a57a089a1bcd37f50f9c1245bc19e9
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:09:07 +0000

    migrate swaync to use mkEnableOption

--- home-manager/programs/wm-wayland/swaync.nix
+++ home-manager/programs/wm-wayland/swaync.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    swaynotificationcenter
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.wm-wayland.swaync;
+in
+{
+  options.usr.wm-wayland.swaync = {
+    enable = lib.mkEnableOption "Swaync";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      swaynotificationcenter
+    ];
+  };
 }
