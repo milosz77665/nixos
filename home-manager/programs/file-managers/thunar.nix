8d1b30c31138fc8d1faca60bc6cebd67a3df83d7
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:56:32 +0000

    migrate thunar to use mkEnableOption

--- home-manager/programs/file-managers/thunar.nix
+++ home-manager/programs/file-managers/thunar.nix
@@ -1,8 +1,22 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    xfce.thunar
-    xfce.thunar-archive-plugin
-    xfce.thunar-volman
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.file-managers.thunar;
+in
+{
+  options.usr.file-managers.thunar = {
+    enable = lib.mkEnableOption "Thunar File Manager";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      xfce.thunar
+      xfce.thunar-archive-plugin
+      xfce.thunar-volman
+    ];
+  };
 }
