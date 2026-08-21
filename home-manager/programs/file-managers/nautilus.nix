3ea14b7a9ea24f6155d4bd8a8154d0b4f2e7a66a
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:55:52 +0000

    migrate nautilus to use mkEnableOption

--- home-manager/programs/file-managers/nautilus.nix
+++ home-manager/programs/file-managers/nautilus.nix
@@ -1,13 +1,27 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    nautilus
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.file-managers.nautilus;
+in
+{
+  options.usr.file-managers.nautilus = {
+    enable = lib.mkEnableOption "Nautilus File Manager";
+  };
 
-  dconf.settings = {
-    "org/gnome/nautilus/preferences" = {
-      default-folder-viewer = "list-view";
-      search-filter-time-type = "last_modified";
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      nautilus
+    ];
+
+    dconf.settings = {
+      "org/gnome/nautilus/preferences" = {
+        default-folder-viewer = "list-view";
+        search-filter-time-type = "last_modified";
+      };
     };
   };
 }
