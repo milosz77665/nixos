ed48057164b26a8c02b9db2cc4d4c3a019b205e9
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:35:57 +0000

    migrate nix on droid modules

--- hosts/nixOnDroid/modules.nix
+++ hosts/nixOnDroid/modules.nix
@@ -1,14 +1,21 @@
+{ ... }:
 {
-  system = [];
-
-  home = [
-    ../../home-manager/programs/cli-tools/atuin.nix
-    ../../home-manager/programs/cli-tools/yazi.nix
-    ../../home-manager/programs/cli-tools/lazygit.nix
-    ../../home-manager/programs/cli-tools/starship.nix
-    ../../home-manager/programs/dev/neovim.nix
-    ../../home-manager/programs/dev/languages/go.nix
-    ../../home-manager/programs/dev/languages/nodejs.nix
-    ../../home-manager/programs/shells/bash.nix
+  imports = [
+    ../../system/nix-on-droid/theme.nix
   ];
+
+  usr.nix-on-droid.theme.enable = true;
+
+  home-manager.config = {
+    usr.cli-tools.yazi.enable = true;
+    usr.cli-tools.atuin.enable = true;
+    usr.cli-tools.lazygit.enable = true;
+    usr.cli-tools.zellij.enable = true;
+    usr.cli-tools.starship.enable = true;
+    usr.dev.neovim.enable = true;
+    usr.dev.direnv.enable = true;
+    usr.dev-languages.go.enable = true;
+    usr.dev-languages.nodejs.enable = true;
+    usr.shells.bash.enable = true;
+  };
 }
