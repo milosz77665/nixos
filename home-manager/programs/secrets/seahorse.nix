f235fa88fd0e506611cfe4a2135c51a3fd418f92
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:02:01 +0000

    migrate seahorse to use mkEnableOption

--- home-manager/programs/secrets/seahorse.nix
+++ home-manager/programs/secrets/seahorse.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    seahorse
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.secrets.seahorse;
+in
+{
+  options.usr.secrets.seahorse = {
+    enable = lib.mkEnableOption "Seahorse";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      seahorse
+    ];
+  };
 }
