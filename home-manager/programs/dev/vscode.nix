7af824657a561ae8bd1a71374af0fc5286754a9a
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:54:08 +0000

    migrate vscode to use mkEnableOption

--- home-manager/programs/dev/vscode.nix
+++ home-manager/programs/dev/vscode.nix
@@ -1,3 +1,17 @@
 {
-  programs.vscode.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.dev.vscode;
+in
+{
+  options.usr.dev.vscode = {
+    enable = lib.mkEnableOption "Visual Studio Code";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.vscode.enable = true;
+  };
 }
