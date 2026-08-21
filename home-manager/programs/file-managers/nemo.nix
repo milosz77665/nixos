55310eb5320cb2672e2901472f488445dd581791
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:56:13 +0000

    migrate nemo to use mkEnableOption

--- home-manager/programs/file-managers/nemo.nix
+++ home-manager/programs/file-managers/nemo.nix
@@ -1,14 +1,28 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    nemo-with-extensions
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.file-managers.nemo;
+in
+{
+  options.usr.file-managers.nemo = {
+    enable = lib.mkEnableOption "Nemo File Manager";
+  };
 
-  dconf.settings = {
-    "org/nemo/preferences" = {
-      show-hidden-files = true;
-      default-folder-viewer = "list-view";
-      show-compact-view-icon-toolbar = false;
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      nemo-with-extensions
+    ];
+
+    dconf.settings = {
+      "org/nemo/preferences" = {
+        show-hidden-files = true;
+        default-folder-viewer = "list-view";
+        show-compact-view-icon-toolbar = false;
+      };
     };
   };
 }
