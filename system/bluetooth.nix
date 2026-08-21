08dc7d4a8d99c3102491cfc3f3417068d6540e91
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:25:29 +0000

    migrate bluetooth to use mkEnableOption

--- system/bluetooth.nix
+++ system/bluetooth.nix
@@ -1,6 +1,20 @@
 {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.sys.bluetooth;
+in
+{
+  options.sys.bluetooth = {
+    enable = lib.mkEnableOption "Bluetooth";
+  };
 
-  services.blueman.enable = true;
+  config = lib.mkIf cfg.enable {
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true;
+
+    services.blueman.enable = true;
+  };
 }
