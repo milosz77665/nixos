416e846e8aa56b22d549788547dabf366a01e6d7
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:13:14 +0000

    migrate rofi to use mkEnableOption

--- home-manager/programs/wm-x11/rofi.nix
+++ home-manager/programs/wm-x11/rofi.nix
@@ -1,4 +1,18 @@
 {
-  programs.rofi.enable = true;
-  xdg.configFile."rofi".source = ../../dotfiles/rofi;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.rofi;
+in
+{
+  options.usr.wm-x11.rofi = {
+    enable = lib.mkEnableOption "Rofi";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.rofi.enable = true;
+    xdg.configFile."rofi".source = ../../dotfiles/rofi;
+  };
 }
