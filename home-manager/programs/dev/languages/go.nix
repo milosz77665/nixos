0820a00e81e51164ddc81478bfec8d6f6938b573
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:51:36 +0000

    migrate go to use mkEnableOption

--- home-manager/programs/dev/languages/go.nix
+++ home-manager/programs/dev/languages/go.nix
@@ -1,6 +1,20 @@
-{ pkgs, pkgsUnstable, ... }:
 {
-  home.packages = with pkgsUnstable; [
-    go
-  ];
+  config,
+  lib,
+  pkgsUnstable,
+  ...
+}:
+let
+  cfg = config.usr.dev-languages.go;
+in
+{
+  options.usr.dev-languages.go = {
+    enable = lib.mkEnableOption "Go";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgsUnstable; [
+      go
+    ];
+  };
 }
