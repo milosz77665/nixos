000134b8fd82b8f7057076b33161bfce6710acd8
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:03:28 +0000

    migrate evince to use mkEnableOption

--- home-manager/programs/tools/evince.nix
+++ home-manager/programs/tools/evince.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    evince
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.tools.evince;
+in
+{
+  options.usr.tools.evince = {
+    enable = lib.mkEnableOption "Evince";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      evince
+    ];
+  };
 }
