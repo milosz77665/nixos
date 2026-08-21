e3a2bbc6bfe96c0ff64f5a4fd623962ba654c987
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:12:59 +0000

    migrate qtile to use mkEnableOption

--- home-manager/programs/wm-x11/qtile.nix
+++ home-manager/programs/wm-x11/qtile.nix
@@ -1,6 +1,20 @@
 {
-  xdg.configFile."qtile" = {
-    source = ../../dotfiles/qtile;
-    recursive = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.qtile;
+in
+{
+  options.usr.wm-x11.qtile = {
+    enable = lib.mkEnableOption "Qtile";
+  };
+
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."qtile" = {
+      source = ../../dotfiles/qtile;
+      recursive = true;
+    };
   };
 }
