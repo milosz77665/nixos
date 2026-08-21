cf69bf86889dfb8c52a2fdb4f64d927aa2641c4d
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:05:58 +0000

    migrate qbittorrent to use mkEnableOption

--- home-manager/programs/tools/qbittorrent.nix
+++ home-manager/programs/tools/qbittorrent.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    qbittorrent
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.tools.qbittorrent;
+in
+{
+  options.usr.tools.qbittorrent = {
+    enable = lib.mkEnableOption "Qbittorrent";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      qbittorrent
+    ];
+  };
 }
