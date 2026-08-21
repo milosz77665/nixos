859c5f9ab969aa56473904de9f886e90a92bb327
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:25:49 +0000

    migrate disk-utils to use mkEnableOption

--- system/disk-utils.nix
+++ system/disk-utils.nix
@@ -1,8 +1,23 @@
 {
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
-  programs.dconf.enable = true;
-  services.tumbler.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.sys.disk-utils;
+in
+{
+  options.sys.disk-utils = {
+    enable = lib.mkEnableOption "Disk utils";
+  };
 
-  boot.supportedFilesystems = [ "ntfs" ];
+  config = lib.mkIf cfg.enable {
+    services.gvfs.enable = true;
+    services.udisks2.enable = true;
+    programs.dconf.enable = true;
+    services.tumbler.enable = true;
+
+    boot.supportedFilesystems = [ "ntfs" ];
+
+  };
 }
