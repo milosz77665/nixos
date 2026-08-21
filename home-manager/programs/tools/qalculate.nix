e8eacc37e89f436da030101f01d30f2236c9a463
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:05:29 +0000

    migrate qalculate to use mkEnableOption

--- home-manager/programs/tools/qalculate.nix
+++ home-manager/programs/tools/qalculate.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    qalculate-gtk
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.tools.qalculate;
+in
+{
+  options.usr.tools.qalculate = {
+    enable = lib.mkEnableOption "Qalculate";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      qalculate-gtk
+    ];
+  };
 }
