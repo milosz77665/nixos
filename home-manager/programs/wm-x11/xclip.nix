17ebd17d8c135a77c866aaac03a28a09bca4e6c9
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:13:30 +0000

    migrate xclip to use mkEnableOption

--- home-manager/programs/wm-x11/xclip.nix
+++ home-manager/programs/wm-x11/xclip.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    xclip
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.xclip;
+in
+{
+  options.usr.wm-x11.xclip = {
+    enable = lib.mkEnableOption "Clipboard";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      xclip
+    ];
+  };
 }
