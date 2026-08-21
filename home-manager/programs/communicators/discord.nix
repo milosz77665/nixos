a99d85870e3ba01e32028cb07db44a8363f29a0a
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:50:22 +0000

    migrate discord to use mkEnableOption

--- home-manager/programs/communicators/discord.nix
+++ home-manager/programs/communicators/discord.nix
@@ -1,3 +1,17 @@
 {
-  programs.discord.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.communicators.discord;
+in
+{
+  options.usr.communicators.discord = {
+    enable = lib.mkEnableOption "Discord";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.discord.enable = true;
+  };
 }
