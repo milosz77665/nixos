4abbd4771ac4f9aff5f303e0cf2e508de4ede510
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:02:38 +0000

    migrate alacritty to use mkEnableOption

--- home-manager/programs/terminals/alacritty.nix
+++ home-manager/programs/terminals/alacritty.nix
@@ -1,3 +1,17 @@
 {
-  programs.alacritty.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.terminals.alacritty;
+in
+{
+  options.usr.terminals.alacritty = {
+    enable = lib.mkEnableOption "Alacritty";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.alacritty.enable = true;
+  };
 }
