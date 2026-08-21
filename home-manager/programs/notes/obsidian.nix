79bb5efe228441d01aad136e3db2ea609bf6003b
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:00:11 +0000

    migrate obsidian to use mkEnableOption

--- home-manager/programs/notes/obsidian.nix
+++ home-manager/programs/notes/obsidian.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    obsidian
-  ];
-}
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.notes.obsidian;
+in
+{
+  options.usr.notes.obsidian = {
+    enable = lib.mkEnableOption "Obsidian";
+  };
 
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      obsidian
+    ];
+  };
+}
