7451c686ec610d74181a0f15513238a5b478a840
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:01:40 +0000

    migrate polkit-gnome to use mkEnableOption

--- home-manager/programs/secrets/polkit-gnome.nix
+++ home-manager/programs/secrets/polkit-gnome.nix
@@ -1,22 +1,36 @@
-{ pkgs, ... }:
 {
-  services.polkit-gnome.enable = true;
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.secrets.polkit-gnome;
+in
+{
+  options.usr.secrets.polkit-gnome = {
+    enable = lib.mkEnableOption "Polkit Gnome";
+  };
 
-  systemd.user.services.polkit-gnome-authentication-agent-1 = {
-    unit = {
-      description = "polkit-gnome-authentication-agent-1";
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-    };
-    service = {
-      type = "simple";
-      execStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      restart = "on-failure";
-      restartSec = 1;
-      timeoutStopSec = 10;
-    };
-    install = {
-      wantedBy = [ "graphical-session.target" ];
+  config = lib.mkIf cfg.enable {
+    services.polkit-gnome.enable = true;
+
+    systemd.user.services.polkit-gnome-authentication-agent-1 = {
+      unit = {
+        description = "polkit-gnome-authentication-agent-1";
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+      };
+      service = {
+        type = "simple";
+        execStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        restart = "on-failure";
+        restartSec = 1;
+        timeoutStopSec = 10;
+      };
+      install = {
+        wantedBy = [ "graphical-session.target" ];
+      };
     };
   };
 }
