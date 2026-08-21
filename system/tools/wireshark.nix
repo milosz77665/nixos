f4c94bc0ab660ebb51259c2f48b7a790e3e9a8dc
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:23:16 +0000

    migrate wireshark to use mkEnableOption

--- system/tools/wireshark.nix
+++ system/tools/wireshark.nix
@@ -1,7 +1,21 @@
-{ pkgs, ... }:
 {
-  programs.wireshark = {
-    enable = true;
-    package = pkgs.wireshark;
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.sys.tools.wireshark;
+in
+{
+  options.sys.tools.wireshark = {
+    enable = lib.mkEnableOption "Wireshark";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.wireshark = {
+      enable = true;
+      package = pkgs.wireshark;
+    };
   };
 }
