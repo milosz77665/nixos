29ad142c9335d482ceac7c5da35d0bd8a8e95ea8
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:53:45 +0000

    migrate postman to use mkEnableOption

--- home-manager/programs/dev/postman.nix
+++ home-manager/programs/dev/postman.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    postman
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.dev.postman;
+in
+{
+  options.usr.dev.postman = {
+    enable = lib.mkEnableOption "Postman";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      postman
+    ];
+  };
 }
