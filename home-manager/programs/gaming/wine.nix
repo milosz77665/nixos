a641653b8cbefa8a9242aa147a29a5f997286be5
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:57:52 +0000

    migrate wine to use mkEnableOption

--- home-manager/programs/gaming/wine.nix
+++ home-manager/programs/gaming/wine.nix
@@ -1,7 +1,21 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    wine
-    winetricks
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.gaming.wine;
+in
+{
+  options.usr.gaming.wine = {
+    enable = lib.mkEnableOption "Wine";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      wine
+      winetricks
+    ];
+  };
 }
