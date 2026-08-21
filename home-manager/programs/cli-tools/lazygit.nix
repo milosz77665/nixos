f52f6d1ef653dcdeb9b64e3b5821f3f77a8a1622
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:48:41 +0000

    migrate lazygit to use mkEnableOption

--- home-manager/programs/cli-tools/lazygit.nix
+++ home-manager/programs/cli-tools/lazygit.nix
@@ -1,7 +1,22 @@
 {
-  programs.lazygit = {
-    enable = true;
-    enableBashIntegration = true;
-    shellWrapperName = "lg";
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.cli-tools.lazygit;
+in
+{
+  options.usr.cli-tools.lazygit = {
+    enable = lib.mkEnableOption "Lazygit";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.lazygit = {
+      enable = true;
+      enableBashIntegration = true;
+      shellWrapperName = "lg";
+    };
   };
 }
+
