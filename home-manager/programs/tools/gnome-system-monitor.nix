fd2a9a68e3a9902d8775f88f548ba742f59db2a3
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:04:26 +0000

    migrate gnome-system-monitor to use mkEnableOption

--- home-manager/programs/tools/gnome-system-monitor.nix
+++ home-manager/programs/tools/gnome-system-monitor.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    gnome-system-monitor
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.tools.gnome-system-monitor;
+in
+{
+  options.usr.tools.gnome-system-monitor = {
+    enable = lib.mkEnableOption "Gnome System Monitor";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gnome-system-monitor
+    ];
+  };
 }
