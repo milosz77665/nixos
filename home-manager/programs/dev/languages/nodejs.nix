9ae4876693cc8ea9ff639ee0663f32b4d07f71f4
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:52:10 +0000

    migrate nodejs to use mkEnableOption

--- home-manager/programs/dev/languages/nodejs.nix
+++ home-manager/programs/dev/languages/nodejs.nix
@@ -1,6 +1,20 @@
-{ pkgs, pkgsUnstable, ... }:
 {
-  home.packages = with pkgsUnstable; [
-    nodejs_22           
-  ];
+  config,
+  lib,
+  pkgsUnstable,
+  ...
+}:
+let
+  cfg = config.usr.dev-languages.nodejs;
+in
+{
+  options.usr.dev-languages.nodejs = {
+    enable = lib.mkEnableOption "Node.js";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgsUnstable; [
+      nodejs_22
+    ];
+  };
 }
