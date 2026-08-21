97f799f2623749422a6b0fad32c7061ef5c52a37
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:47:33 +0000

    migrate atuin to use mkEnableOption

--- home-manager/programs/cli-tools/atuin.nix
+++ home-manager/programs/cli-tools/atuin.nix
@@ -1,7 +1,21 @@
 {
-  programs.atuin = {
-    enable = true;
-    enableBashIntegration = true;
-    flags = [ "--disable-up-arrow" ];
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.cli-tools.atuin;
+in
+{
+  options.usr.cli-tools.atuin = {
+    enable = lib.mkEnableOption "Atuin";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.atuin = {
+      enable = true;
+      enableBashIntegration = true;
+      flags = [ "--disable-up-arrow" ];
+    };
   };
 }
