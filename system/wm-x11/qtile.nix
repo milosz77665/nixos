44a2830c4f5c3af0c4c9418e4da76446830493f7
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:24:24 +0000

    migrate qtile to use mkEnableOption

--- system/wm-x11/qtile.nix
+++ system/wm-x11/qtile.nix
@@ -1,20 +1,34 @@
-{ pkgs, pkgsUnstable, ... }:
 {
-  programs.gdk-pixbuf.modulePackages = [ pkgsUnstable.librsvg ];
+  config,
+  lib,
+  pkgsUnstable,
+  ...
+}:
+let
+  cfg = config.sys.wm-x11.qtile;
+in
+{
+  options.sys.wm-wayland.qtile = {
+    enable = lib.mkEnableOption "Qtile";
+  };
 
-  services.xserver = {
-    enable = true;
-    windowManager.qtile = {
+  config = lib.mkIf cfg.enable {
+    programs.gdk-pixbuf.modulePackages = [ pkgsUnstable.librsvg ];
+
+    services.xserver = {
       enable = true;
-      package = pkgsUnstable.python3.pkgs.qtile;
-      extraPackages = 
-        python3Packages: with python3Packages; [
-          qtile-extras
-          psutil
-          pywal
-          xcffib
-          dbus-fast
-        ];
+      windowManager.qtile = {
+        enable = true;
+        package = pkgsUnstable.python3.pkgs.qtile;
+        extraPackages =
+          python3Packages: with python3Packages; [
+            qtile-extras
+            psutil
+            pywal
+            xcffib
+            dbus-fast
+          ];
+      };
     };
   };
 }
