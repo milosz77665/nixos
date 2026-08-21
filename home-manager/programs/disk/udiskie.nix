737e001b45dd01dd89db1a6e40f42a1b1c94bd5a
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:55:31 +0000

    migrate udiskie to use mkEnableOption

--- home-manager/programs/disk/udiskie.nix
+++ home-manager/programs/disk/udiskie.nix
@@ -1,6 +1,20 @@
 {
-  services.udiskie = {
-    enable = true;
-    tray = "always";
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.disk.udiskie;
+in
+{
+  options.usr.disk.udiskie = {
+    enable = lib.mkEnableOption "Udiskie";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.udiskie = {
+      enable = true;
+      tray = "always";
+    };
   };
 }
