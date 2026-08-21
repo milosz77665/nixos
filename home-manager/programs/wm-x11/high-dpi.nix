d5fd409ce07f00de9e2e426d231ee3344fa68577
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:12:06 +0000

    migrate high-dpi to use mkEnableOption

--- home-manager/programs/wm-x11/high-dpi.nix
+++ home-manager/programs/wm-x11/high-dpi.nix
@@ -1,5 +1,19 @@
 {
-  xresources.properties = {
-    "Xft.dpi" = 120;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.high-dpi;
+in
+{
+  options.usr.wm-x11.high-dpi = {
+    enable = lib.mkEnableOption "High dpi";
+  };
+
+  config = lib.mkIf cfg.enable {
+    xresources.properties = {
+      "Xft.dpi" = 120;
+    };
   };
 }
