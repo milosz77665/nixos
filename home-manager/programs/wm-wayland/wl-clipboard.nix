17018b13dae2b460f8dcdc8521bc538e6ad02753
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:10:13 +0000

    migrate wl-clipboard to use mkEnableOption

--- home-manager/programs/wm-wayland/wl-clipboard.nix
+++ home-manager/programs/wm-wayland/wl-clipboard.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    wl-clipboard
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.wm-wayland.clipboard;
+in
+{
+  options.usr.wm-wayland.clipboard = {
+    enable = lib.mkEnableOption "Clipboard";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      wl-clipboard
+    ];
+  };
 }
