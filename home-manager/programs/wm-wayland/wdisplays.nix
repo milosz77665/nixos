7166b27b7a1b60ab39d98ebf4fbeaaf19997875e
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:09:49 +0000

    migrate wdisplays to use mkEnableOption

--- home-manager/programs/wm-wayland/wdisplays.nix
+++ home-manager/programs/wm-wayland/wdisplays.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    wdisplays
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.wm-wayland.wdisplays;
+in
+{
+  options.usr.wm-wayland.wdisplays = {
+    enable = lib.mkEnableOption "Wdisplays";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      wdisplays
+    ];
+  };
 }
