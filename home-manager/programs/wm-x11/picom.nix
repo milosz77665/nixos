bb306ee13a7944e712779342c5f8e95dfec8ebbc
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:12:24 +0000

    migrate picom to use mkEnableOption

--- home-manager/programs/wm-x11/picom.nix
+++ home-manager/programs/wm-x11/picom.nix
@@ -1,42 +1,56 @@
 {
-  services.picom = {
-    enable = true;
-    # backend = "glx";
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.picom;
+in
+{
+  options.usr.wm-x11.picom = {
+    enable = lib.mkEnableOption "Picom";
+  };
 
-    vSync = true;
-    activeOpacity = 0.90;
-    inactiveOpacity = 0.90;
-    opacityRules = [
-      "100:class_g = 'i3lock'"
-      "100:name = 'qtile_bar'"
-      "100:QTILE_INTERNAL@:32c = 1"
-    ];
+  config = lib.mkIf cfg.enable {
+    services.picom = {
+      enable = true;
+      # backend = "glx";
 
-    fade = true;
-    fadeSteps = [
-      0.03
-      0.03
-    ];
-    fadeDelta = 4;
-
-    settings = {
-      inactive-dim = 0.0;
-      unredir-if-possible = false;
-      inactive-opacity-override = false;
-
-      blur = {
-        method = "dual_kawase";
-        strength = 3;
-      };
-
-      blur-background-exclude = [
-        "class_g = 'i3lock'"
-        "name = 'qtile_bar'"
-        "QTILE_INTERNAL@:32c = 1"
-        "window_type = 'dock'"
-        "window_type = 'desktop'"
-        "_GTK_FRAME_EXTENTS@:c"
+      vSync = true;
+      activeOpacity = 0.90;
+      inactiveOpacity = 0.90;
+      opacityRules = [
+        "100:class_g = 'i3lock'"
+        "100:name = 'qtile_bar'"
+        "100:QTILE_INTERNAL@:32c = 1"
       ];
+
+      fade = true;
+      fadeSteps = [
+        0.03
+        0.03
+      ];
+      fadeDelta = 4;
+
+      settings = {
+        inactive-dim = 0.0;
+        unredir-if-possible = false;
+        inactive-opacity-override = false;
+
+        blur = {
+          method = "dual_kawase";
+          strength = 3;
+        };
+
+        blur-background-exclude = [
+          "class_g = 'i3lock'"
+          "name = 'qtile_bar'"
+          "QTILE_INTERNAL@:32c = 1"
+          "window_type = 'dock'"
+          "window_type = 'desktop'"
+          "_GTK_FRAME_EXTENTS@:c"
+        ];
+      };
     };
   };
 }
