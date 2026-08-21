9a38d201e7365082e55001280419efa1d752bd5e
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:09:28 +0000

    migrate waybar to use mkEnableOption

--- home-manager/programs/wm-wayland/waybar.nix
+++ home-manager/programs/wm-wayland/waybar.nix
@@ -1,10 +1,24 @@
 {
-  programs.waybar = {
-    enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-wayland.waybar;
+in
+{
+  options.usr.wm-wayland.waybar = {
+    enable = lib.mkEnableOption "Waybar";
   };
 
-  xdg.configFile."waybar" = {
-    source = ../../dotfiles/waybar;
-    recursive = true;
+  config = lib.mkIf cfg.enable {
+    programs.waybar = {
+      enable = true;
+    };
+
+    xdg.configFile."waybar" = {
+      source = ../../dotfiles/waybar;
+      recursive = true;
+    };
   };
 }
