54d5c5f75d8d3994ee1a1e244da169aba6d603e0
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:03:59 +0000

    migrate gimp to use mkEnableOption

--- home-manager/programs/tools/gimp.nix
+++ home-manager/programs/tools/gimp.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    gimp
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.tools.gimp;
+in
+{
+  options.usr.tools.gimp = {
+    enable = lib.mkEnableOption "Gimp";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gimp
+    ];
+  };
 }
