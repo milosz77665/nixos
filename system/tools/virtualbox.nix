d6f09c7105dcd01ae638034ec981b5744b9f38a2
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:22:50 +0000

    migrate virtualbox to use mkEnableOption

--- system/tools/virtualbox.nix
+++ system/tools/virtualbox.nix
@@ -1,3 +1,17 @@
 {
-  virtualisation.virtualbox.host.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.sys.tools.virtualbox;
+in
+{
+  options.sys.tools.virtualbox = {
+    enable = lib.mkEnableOption "Virtualbox";
+  };
+
+  config = lib.mkIf cfg.enable {
+    virtualisation.virtualbox.host.enable = true;
+  };
 }
