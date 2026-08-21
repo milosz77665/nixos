6fe08a27f8abe173b66939403b878b8415c4ebd5
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:08:27 +0000

    migrate swaybg to use mkEnableOption

--- home-manager/programs/wm-wayland/swaybg.nix
+++ home-manager/programs/wm-wayland/swaybg.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    swaybg
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.wm-wayland.swaybg;
+in
+{
+  options.usr.wm-wayland.swaybg = {
+    enable = lib.mkEnableOption "Swaybg";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      swaybg
+    ];
+  };
 }
