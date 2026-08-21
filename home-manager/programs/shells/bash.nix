a566dbf04903d38747619e4a4a4dc28b1e7f428d
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:02:16 +0000

    migrate bash to use mkEnableOption

--- home-manager/programs/shells/bash.nix
+++ home-manager/programs/shells/bash.nix
@@ -1,9 +1,23 @@
 {
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    bashrcExtra = ''
-      export NIX_MANAGED="1"
-    '';
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.shells.bash;
+in
+{
+  options.usr.shells.bash = {
+    enable = lib.mkEnableOption "Bash";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.bash = {
+      enable = true;
+      enableCompletion = true;
+      bashrcExtra = ''
+        export NIX_MANAGED="1"
+      '';
+    };
   };
 }
