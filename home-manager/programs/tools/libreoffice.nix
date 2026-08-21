4a8c921829284dc35d9ee7a60d6a8ed93a482f3f
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:04:50 +0000

    migrate libreoffice to use mkEnableOption

--- home-manager/programs/tools/libreoffice.nix
+++ home-manager/programs/tools/libreoffice.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    libreoffice
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.tools.libreoffice;
+in
+{
+  options.usr.tools.libreoffice = {
+    enable = lib.mkEnableOption "Libreoffice";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      libreoffice
+    ];
+  };
 }
