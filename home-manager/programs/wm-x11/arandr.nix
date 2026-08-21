914ba0eadfacf7662c10a842c0482d98d31fcdb7
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:10:42 +0000

    migrate arandr to use mkEnableOption

--- home-manager/programs/wm-x11/arandr.nix
+++ home-manager/programs/wm-x11/arandr.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    arandr
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.arandr;
+in
+{
+  options.usr.wm-x11.arandr = {
+    enable = lib.mkEnableOption "Arandr";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      arandr
+    ];
+  };
 }
