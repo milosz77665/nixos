174e960abf7345fab28d296ee7dc25de35e8a379
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:48:17 +0000

    migrate lazydocker to use mkEnableOption

--- home-manager/programs/cli-tools/lazydocker.nix
+++ home-manager/programs/cli-tools/lazydocker.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    lazydocker
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.cli-tools.lazydocker;
+in
+{
+  options.usr.cli-tools.lazydocker = {
+    enable = lib.mkEnableOption "Lazydocker";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      lazydocker
+    ];
+  };
 }
