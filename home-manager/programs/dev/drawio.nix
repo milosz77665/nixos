0c00965b4a77affd92ed794d8c6a6b3e2e5bb682
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:52:52 +0000

    migrate drawio to use mkEnableOption

--- home-manager/programs/dev/drawio.nix
+++ home-manager/programs/dev/drawio.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    drawio
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.dev.drawio;
+in
+{
+  options.usr.dev.drawio = {
+    enable = lib.mkEnableOption "Drawio";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      drawio
+    ];
+  };
 }
