26fe128f23ec18103e95e3f0bbc0fedcc2e4ed5d
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:01:15 +0000

    migrate dunst to use mkEnableOption

--- home-manager/programs/notifications/dunst.nix
+++ home-manager/programs/notifications/dunst.nix
@@ -1,13 +1,27 @@
 {
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        offset = "(10,50)";
-        origin = "top-right";
-        transparency = 10;
-        frame_color = "#eceff4";
-        font = "FiraCode Nerd Font 10";
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.notifications.dunst;
+in
+{
+  options.usr.notifications.dunst = {
+    enable = lib.mkEnableOption "Dunst";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.dunst = {
+      enable = true;
+      settings = {
+        global = {
+          offset = "(10,50)";
+          origin = "top-right";
+          transparency = 10;
+          frame_color = "#eceff4";
+          font = "FiraCode Nerd Font 10";
+        };
       };
     };
   };
