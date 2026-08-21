8083804d5ea071a1340f38bcb41467b4de0305fb
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:11:05 +0000

    migrate feh to use mkEnableOption

--- home-manager/programs/wm-x11/feh.nix
+++ home-manager/programs/wm-x11/feh.nix
@@ -1,3 +1,17 @@
 {
-  programs.feh.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.feh;
+in
+{
+  options.usr.wm-x11.feh = {
+    enable = lib.mkEnableOption "Feh";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.feh.enable = true;
+  };
 }
