771f5038756ead52a37f9f1ad47455ebbb1c10a9
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:23:34 +0000

    migrate swaylock to use mkEnableOption

--- system/wm-wayland/swaylock.nix
+++ system/wm-wayland/swaylock.nix
@@ -1,3 +1,17 @@
 {
-  security.pam.services.swaylock = { };
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.sys.wm-wayland.swaylock;
+in
+{
+  options.sys.wm-wayland.swaylock = {
+    enable = lib.mkEnableOption "Swaylock";
+  };
+
+  config = lib.mkIf cfg.enable {
+    security.pam.services.swaylock = { };
+  };
 }
