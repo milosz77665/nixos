f35eedf4546d31d87116ecf95038807a307f1232
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:54:39 +0000

    migrate baobab to use mkEnableOption

--- home-manager/programs/disk/baobab.nix
+++ home-manager/programs/disk/baobab.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    baobab
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.disk.baobab;
+in
+{
+  options.usr.disk.baobab = {
+    enable = lib.mkEnableOption "Baobab";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      baobab
+    ];
+  };
 }
