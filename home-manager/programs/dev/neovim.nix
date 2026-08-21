6bcdaa4766e18be8f570f30849523d183b0ab971
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:53:23 +0000

    migrate neovim to use mkEnableOption

--- home-manager/programs/dev/neovim.nix
+++ home-manager/programs/dev/neovim.nix
@@ -1,18 +1,35 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    neovim
-    tree-sitter
-    # Lang servers
-    gopls
-    typescript-language-server
-    vue-language-server
-    lua-language-server
-    nixd
-    vscode-langservers-extracted
-    # Formatters
-    stylua
-    nixfmt-rfc-style
-    kdlfmt
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.dev.neovim;
+in
+{
+  options.usr.dev.neovim = {
+    enable = lib.mkEnableOption "Neovim";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      neovim
+      tree-sitter
+      # Lang servers
+      gopls
+      typescript-language-server
+      vue-language-server
+      lua-language-server
+      nixd
+      vscode-langservers-extracted
+      tailwindcss-language-server
+      # Formatters
+      stylua
+      nixfmt-rfc-style
+      kdlfmt
+      prettierd
+      golangci-lint
+    ];
+  };
 }
