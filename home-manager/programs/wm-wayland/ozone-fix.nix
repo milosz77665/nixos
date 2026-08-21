62df2848a0858c59b94d756f02ba7b2a05f31ae7
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:08:06 +0000

    migrate ozone-fix to use mkEnableOption

--- home-manager/programs/wm-wayland/ozone-fix.nix
+++ home-manager/programs/wm-wayland/ozone-fix.nix
@@ -1,5 +1,19 @@
 {
-  home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-wayland.ozone;
+in
+{
+  options.usr.wm-wayland.ozone = {
+    enable = lib.mkEnableOption "Ozone";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
   };
 }
