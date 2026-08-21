fe8a9a0521f66ef7f438f4cb984249b9ea4b2d96
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:20:01 +0000

    migrate brave to use mkEnableOption

--- home-manager/programs/browsers/brave.nix
+++ home-manager/programs/browsers/brave.nix
@@ -1,9 +1,23 @@
 {
-  programs.brave = {
-    enable = true;
-    commandLineArgs = [
-      "--force-dark-mode"
-      "--restore-last-session"
-    ];
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.browser.brave;
+in
+{
+  options.usr.browser.brave = {
+    enable = lib.mkEnableOption "Brave Browser";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.brave = {
+      enable = true;
+      commandLineArgs = [
+        "--force-dark-mode"
+        "--restore-last-session"
+      ];
+    };
   };
 }
--- system/browser-policies/brave.nix
+++ system/browser-policies/brave.nix
@@ -1,19 +1,33 @@
 {
-  environment.etc."brave/policies/managed/extensions.json".text = builtins.toJSON {
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.sys.browser-policies.brave;
+in
+{
+  options.sys.browser-policies.brave = {
+    enable = lib.mkEnableOption "Brave policies";
+  };
 
-    ExtensionInstallForcelist = [
-      "cfhdojbkjhnklbpkdaibdccddilifddb" # Adblock Plus
-      "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
-      "lmhkpmbekcpmknklioeibfkpmmfibljd" # Redux DevTools
-      "lomlmaamgdjplnhhgnoajlbnlgnpkobl" # Video Popout
-      "nhdogjmejiglipccpnnnanhbledajbpd" # Vue.js devtools
-      "jchobbjgibcahbheicfocecmhocglkco" # URLs Cleaner
-      "lphicbbhfmllgmomkkhjfkpbdlncafbn" # LetyShops
-      "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-    ];
+  config = lib.mkIf cfg.enable {
+    environment.etc."brave/policies/managed/extensions.json".text = builtins.toJSON {
 
-    RestoreOnStartup = 5;
-    BraveRewardsDisabled = true;
-    BraveWalletDisabled = true;
+      ExtensionInstallForcelist = [
+        "cfhdojbkjhnklbpkdaibdccddilifddb" # Adblock Plus
+        "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
+        "lmhkpmbekcpmknklioeibfkpmmfibljd" # Redux DevTools
+        "lomlmaamgdjplnhhgnoajlbnlgnpkobl" # Video Popout
+        "nhdogjmejiglipccpnnnanhbledajbpd" # Vue.js devtools
+        "jchobbjgibcahbheicfocecmhocglkco" # URLs Cleaner
+        "lphicbbhfmllgmomkkhjfkpbdlncafbn" # LetyShops
+        "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
+      ];
+
+      RestoreOnStartup = 5;
+      BraveRewardsDisabled = true;
+      BraveWalletDisabled = true;
+    };
   };
 }
