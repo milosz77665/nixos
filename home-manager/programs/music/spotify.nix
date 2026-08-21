0d1b6cc429fed6ea2c8170e77bf930ff1f949f80
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:58:25 +0000

    migrate spotify to use mkEnableOption

--- home-manager/programs/music/spotify.nix
+++ home-manager/programs/music/spotify.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    spotify
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.music.spotify;
+in
+{
+  options.usr.music.spotify = {
+    enable = lib.mkEnableOption "Spotify";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      spotify
+    ];
+  };
 }
