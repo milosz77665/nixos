4bcde005f295d2f16aa9078c075426aca03abf6e
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:55:08 +0000

    migrate gnome-disk-utility to use mkEnableOption

--- home-manager/programs/disk/gnome-disk-utility.nix
+++ home-manager/programs/disk/gnome-disk-utility.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    gnome-disk-utility
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.disk.gnome-disk-utility;
+in
+{
+  options.usr.disk.gnome-disk-utility = {
+    enable = lib.mkEnableOption "Gnome Disk Utility";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gnome-disk-utility
+    ];
+  };
 }
