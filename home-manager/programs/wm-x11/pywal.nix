82080284cc04bcd1e6cc3385b87da52bcb2bb5c7
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 17:12:39 +0000

    migrate pywal to use mkEnableOption

--- home-manager/programs/wm-x11/pywal.nix
+++ home-manager/programs/wm-x11/pywal.nix
@@ -1,3 +1,17 @@
 {
-  programs.pywal.enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.wm-x11.pywal;
+in
+{
+  options.usr.wm-x11.pywal = {
+    enable = lib.mkEnableOption "Pywal";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.pywal.enable = true;
+  };
 }
