8981fad4df695a12e33273d59a917bf38c16b5e9
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:57:02 +0000

    migrate heroic to use mkEnableOption

--- home-manager/programs/gaming/heroic.nix
+++ home-manager/programs/gaming/heroic.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    heroic
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.gaming.heroic;
+in
+{
+  options.usr.gaming.heroic = {
+    enable = lib.mkEnableOption "Heroic";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      heroic
+    ];
+  };
 }
