701aee6b87332012c324eb8c03d2562b54cbc3df
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:47:06 +0000

    migrate firefox to use mkEnableOption

--- home-manager/programs/browsers/firefox.nix
+++ home-manager/programs/browsers/firefox.nix
@@ -1,50 +1,63 @@
-{ config, pkgs, ... }:
 {
-  programs.firefox = {
-    enable = true;
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.usr.browser.firefox;
+in
+{
+  options.usr.browser.firefox = {
+    enable = lib.mkEnableOption "Firefox Browser";
+  };
 
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
+  config = lib.mkIf cfg.enable {
+    programs.firefox = {
+      enable = true;
+
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = false;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "never";
       };
-      DisablePocket = true;
-      DisableFirefoxAccounts = true;
-      DisableAccounts = false;
-      DisableFirefoxScreenshots = true;
-      OverrideFirstRunPage = "";
-      OverridePostUpdatePage = "";
-      DontCheckDefaultBrowser = true;
-      DisplayBookmarksToolbar = "never";
-    };
 
-    profiles.default = {
-      id = 0;
-      isDefault = true;
+      profiles.default = {
+        id = 0;
+        isDefault = true;
 
-      settings = {
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-        "privacy.globalprivacycontrol.enabled" = true;
+        settings = {
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
+          "privacy.globalprivacycontrol.enabled" = true;
 
-        "datareporting.policy.dataSubmissionEnabled" = false;
-        "datareporting.healthreport.uploadEnabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.server" = "data:,";
-        "browser.ping-centre.telemetry" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.server" = "data:,";
+          "browser.ping-centre.telemetry" = false;
 
-        "browser.search.suggest.enabled" = false;
-        "browser.urlbar.suggest.searches" = false;
-        "browser.urlbar.speculativeConnect.enabled" = false;
+          "browser.search.suggest.enabled" = false;
+          "browser.urlbar.suggest.searches" = false;
+          "browser.urlbar.speculativeConnect.enabled" = false;
 
-        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-        "devtools.theme" = "dark";
-        "ui.systemUsesDarkTheme" = 1;
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "devtools.theme" = "dark";
+          "ui.systemUsesDarkTheme" = 1;
+        };
       };
     };
   };
