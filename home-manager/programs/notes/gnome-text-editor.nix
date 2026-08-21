effdf95db962e46ec14ef3c8bc58b997d0bb868a
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-21 16:59:40 +0000

    migrate gnome-text-editor to use mkEnableOption

--- home-manager/programs/notes/gnome-text-editor.nix
+++ home-manager/programs/notes/gnome-text-editor.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    gnome-text-editor
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.usr.notes.gnome-text-editor;
+in
+{
+  options.usr.notes.gnome-text-editor = {
+    enable = lib.mkEnableOption "Gnome Text Editor";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gnome-text-editor
+    ];
+  };
 }
