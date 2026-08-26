4b93937c0cd783cbb6c6c78d25446d6f55f886b4
Miłosz Marchewka <milosz77665@gmail.com>
2026-08-26 23:01:43 +0200

    fix issue with unclosed git options

--- home-manager/programs/dev/git.nix
+++ home-manager/programs/dev/git.nix
@@ -10,14 +10,14 @@ in
 {
   options.usr.dev.git = {
     enable = lib.mkEnableOption "Git";
+  };
 
-    config = lib.mkIf cfg.enable {
-      programs.git = {
-        enable = true;
-        settings.user = {
-          name = userConfig.git.username;
-          email = userConfig.git.email;
-        };
+  config = lib.mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      settings.user = {
+        name = userConfig.git.username;
+        email = userConfig.git.email;
       };
     };
   };
